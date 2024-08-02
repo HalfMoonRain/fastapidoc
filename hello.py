ae1bfbe0c0cfa2a97ebdf4869edf18342048b29d
junwoo <junwoo1357@gmail.com>
2024-08-02 20:51:39 +0900

    hello.py

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body, Header
 
 app = FastAPI()
 
-@app.get('/hi')
-def greet():
-    return "Hello, World?"
+@app.get('/agent')
+def greet(user_agent:str = Header()):
+    return user_agent
 
 if __name__ == "__main__":
     import uvicorn
