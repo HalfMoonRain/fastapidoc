934c9c995534a7685064cf9b12a3163507a67047
junwoo <junwoo1357@gmail.com>
2024-08-03 16:40:45 +0900

    모델관리연습

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI, Body, Header
+import datetime
+import json
+from fastapi import FastAPI, Body, Header, Response
+from fastapi.encoders import jsonable_encoder
+import pytest
 
 app = FastAPI()
 
@@ -6,6 +10,30 @@ app = FastAPI()
 def greet(user_agent:str = Header()):
     return user_agent
 
+@app.get("/happy")
+def happy(status_code=200):
+    return ":)"
+
+@app.get("/header/{name}/{value}")
+def header(name:str, value: str, response: Response):
+    response.headers[name] = value
+    return 'normal body'
+
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("hello:app", reload=True)
+
+
+# @pytest.fixture
+# def data():
+#     return datetime.datetime.now()
+
+# def test_json_dump(data):
+#     with pytest.raises(Exception):
+#         _ = json.dumps(data)
+
+# def test_encoder(data):
+#     out = jsonable_encoder(data)
+#     assert out
+#     json_out = json.dumps(out)
+#     assert json_out
---
+++ model/tag.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from pydantic import BaseModel
+
+
+class TagIn(BaseModel):
+    tag: str
+
+class Tag(BaseModel):
+    tag: str
+    created: datetime
+    secret: str
+
+class TagOut(BaseModel):
+    tag: str
+    created: datetime
