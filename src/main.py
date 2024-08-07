625126df995e662077229f3c42393ba748df6ab2
junwoo <junwoo1357@gmail.com>
2024-08-07 21:06:00 +0900

    src web

---
+++ src/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+
+from src.web import explorer
+
+
+app = FastAPI()
+
+
+app.include_router(explorer.router)
+
+@app.get("/")
+def top():
+    return "top here"
+
+
+@app.get('/echo/{thing}')
+def echo(thing):
+    return f"echoing {thing}"
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", reload=True)
+
+
---
+++ src/web/explorer.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+
+
+router = APIRouter(prefix="/explorer")
+
+@router.get("/")
+def top():
+    return "top explorer endpoint"
+
