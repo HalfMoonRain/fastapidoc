27552556e9f1cc1d7c5747398aae5363a174c8dd
junwoo <junwoo1357@gmail.com>
2024-08-07 22:14:15 +0900

    web explorer || new endpoint

--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,9 +1,15 @@
 from fastapi import APIRouter
 
+from src.model.explorer import Explorer
+import src.fake.explorer as service
 
 router = APIRouter(prefix="/explorer")
 
 @router.get("/")
-def top():
-    return "top explorer endpoint"
+def get_all() -> list[Explorer] | None :
+    return service.get_all()
+
+@router.get("/{name}")
+def get_one(name) -> Explorer | None:
+    return service.get_one(name)
 
