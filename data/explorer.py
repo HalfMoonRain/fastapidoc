209a5ea364a2a2f7cd595b7b905ea4680bdfb743
junwoo <junwoo1357@gmail.com>
2024-08-11 14:27:18 +0900

    db explorer

---
+++ data/explorer.py
@@ -0,0 +1,51 @@
+from __init__ import curs
+from model.explorer import Explorer
+
+curs.execute("""create table if not exists explorer(
+                name text primary key,
+                country text,
+                description text)""")
+
+def row_to_model(row: tuple) -> Explorer:
+    return Explorer(name=row[0], country=row[1], description=row[2])
+
+def model_to_dict(explorer: Explorer) -> dict:
+    return explorer.model_dump() if explorer else None
+
+def get_one(name: str) -> Explorer:
+    qry = "select * from explorer where name =:name"
+    params = {"name" : name}
+    curs.execute(qry, params)
+    return row_to_model(curs.fetchall())
+
+def get_all() -> list[Explorer] :
+    qry = "select * from explorer"
+    curs.execute(qry)
+    return [row_to_model(row) for row in curs.fetchall()]
+
+def create(explorer: Explorer) -> Explorer:
+    qry = """insert into explorer (name, country, description)
+            values (:name, :country, :description)"""
+    params = model_to_dict(explorer)
+    _ = curs.execute(qry, params)
+    return get_one(explorer.name)
+
+def modify(name: str, explorer: Explorer) -> Explorer:
+    qry = """
+        update explorer
+        set country = :country,
+        name = :name,
+        description = :description
+        where name = :name """
+    params = model_to_dict(explorer)
+    params["name_orig"] = explorer.name
+    _ = curs.execute(qry, params)
+    explorer2 = get_one(explorer.name)
+    return explorer2
+
+def delete(explorer: Explorer) -> bool:
+    qry = "delete from explorer where name = :name"
+    params = {"name" : explorer.name}
+    res = curs.execute(qry, params)
+    return bool(res)
+
--- src/service/creature.py
+++ src/service/creature.py
@@ -1,5 +1,5 @@
 from model.creature import Creature
-import fake.creature as data
+import creature as data
 
 def get_all() -> list[Creature]:
     return data.get_all()
