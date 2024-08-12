49030ccc5c190578c194cd759537dfd72a2123b3
junwoo <junwoo1357@gmail.com>
2024-08-12 22:05:19 +0900

    add id, pw

--- src/auth.py
+++ src/auth.py
@@ -1,15 +1,23 @@
 import uvicorn
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 
 app = FastAPI()
+
+secret_user: str = "newphone"
+secret_password: str = "whodis?"
 
 basic = HTTPBasic()
 
 @app.get("/who")
 def get_user(
-    creds: HTTPBasicCredentials = Depends(basic)):
-    return {"username": creds.username, "password" : creds.password}
+    creds: HTTPBasicCredentials = Depends(basic)) -> dict:
+    if (creds.username == secret_user and
+        creds.password == secret_password):
+        return {"username": creds.username,
+                "password": creds.password}
+    raise HTTPException(status_code=401, detail="hey!")
+
 
 if __name__ == "__main__":
     uvicorn.run("auth:app", reload=True)
