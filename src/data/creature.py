cd5dbb1647fb353a8ed44b9fd40329e211a88263
junwoo <junwoo1357@gmail.com>
2024-08-12 21:31:04 +0900

    fix textbook
    책에 있는 내용이랑 실제 작동하는 코드랑 많이 다름...

--- src/data/creature.py
+++ src/data/creature.py
@@ -1,54 +1,99 @@
-from . import conn, curs
+from .init import conn, curs, IntegrityError
 from model.creature import Creature
+from error import Missing, Duplicate
 
-curs.execute("""create table if not exists creature(
-    name text primary key,
-    description text,
-    country text,
-    area text,
-    aka text) """)
+curs.execute(
+    """create table if not exists creature(
+                name text primary key,
+                description text,
+                country text,
+                area text,
+                aka text)"""
+)
+
 
 def row_to_model(row: tuple) -> Creature:
-    (name, description, country, area, aka) = row
-    return Creature(name, description, country, area, aka)
+    name, description, country, area, aka = row
+    return Creature(
+        name=name,
+        description=description,
+        country=country,
+        area=area,
+        aka=aka,
+    )
+
 
 def model_to_dict(creature: Creature) -> dict:
     return creature.model_dump()
 
+
 def get_one(name: str) -> Creature:
-    qry = "select * from creature where name= :name"
-    params = {"name" : name}
+    qry = "select * from creature where name=:name"
+    params = {"name": name}
     curs.execute(qry, params)
-    return row_to_model(curs.fetchone())
+    row = curs.fetchone()
+    if row:
+        return row_to_model(row)
+    else:
+        raise Missing(msg=f"Creature {name} not found")
+
 
 def get_all() -> list[Creature]:
     qry = "select * from creature"
     curs.execute(qry)
     return [row_to_model(row) for row in curs.fetchall()]
 
+
 def create(creature: Creature) -> Creature:
-    qry = "insert into creature values" \
-        " (:name, :description, :country, :area, :aka)"
+    qry = "insert into creature values" " (:name, :description, :country, :area, :aka)"
     params = model_to_dict(creature)
-    curs.execute(qry, params)
+    try:
+        curs.execute(qry, params)
+    except IntegrityError:
+        raise Duplicate(msg=f"Explorer {creature.name} already exists")
     return get_one(creature.name)
 
-def modify(creature: Creature) -> Creature:
+
+def modify(name: str, creature: Creature) -> Creature:
     qry = """update creature
-            set country = :country,
-                name = :name,
-                desciprtion = :desciption,
-                area = :area,
-                aka = :aka
-            where name = :name_orig
-    """
+             set country=:country,
+                 name=:name,
+                 description=:description,
+                 area=:area,
+                 aka=:aka
+             where name=:name_orig"""
     params = model_to_dict(creature)
     params["name_orig"] = creature.name
-    _ = curs.execute(qry, params)
-    return get_one(create.name)
+    curs.execute(qry, params)
+    if curs.rowcount == 1:
+        return get_one(creature.name)
+    else:
+        raise Missing(msg=f"Creature {name} not found")
 
-def delete(creature: Creature) -> bool:
+
+def replace(name: str, creature: Creature) -> Creature:
+    qry = """update creature
+             set country=:country,
+                 name=:name,
+                 description=:description,
+                 area=:area,
+                 aka=:aka
+             where name=:name_orig"""
+    params = model_to_dict(creature)
+    params["name_orig"] = creature.name
+    curs.execute(qry, params)
+    if curs.rowcount == 1:
+        return get_one(creature.name)
+    else:
+        raise Missing(msg=f"Creature {name} not found")
+
+
+def delete(name: str) -> bool:
+    if not name:
+        return False
     qry = "delete from creature where name = :name"
-    params = {"name" : creature.name}
-    res = curs.execute(qry, params)
-    return bool(res)
+    params = {"name": name}
+    curs.execute(qry, params)
+    if curs.rowcount != 1:
+        raise Missing(msg=f"Explorer {name} not found")
+    return True
--- src/data/explorer.py
+++ src/data/explorer.py
@@ -1,69 +1,94 @@
-from sqlite3 import IntegrityError
-from error import Duplicate, Missing
-from . import curs
+from .init import curs, IntegrityError
 from model.explorer import Explorer
+from error import Missing, Duplicate
 
-curs.execute("""create table if not exists explorer(
+curs.execute(
+    """create table if not exists explorer(
                 name text primary key,
                 country text,
-                description text)""")
+                description text)"""
+)
+
 
 def row_to_model(row: tuple) -> Explorer:
-    return Explorer(name=row[0], country=row[1], description=row[2])
+    name, country, description = row
+    return Explorer(name=name, country=country, description=description)
+
 
 def model_to_dict(explorer: Explorer) -> dict:
-    return explorer.model_dump() if explorer else None
+    return explorer.model_dump()
+
 
 def get_one(name: str) -> Explorer:
-    qry = "select * from explorer where name =:name"
-    params = {"name" : name}
+    qry = "select * from explorer where name=:name"
+    params = {"name": name}
     curs.execute(qry, params)
     row = curs.fetchone()
     if row:
         return row_to_model(row)
     else:
-        raise Missing(f"Explorer {name} not found")
+        raise Missing(msg=f"Explorer {name} not found")
 
-def get_all() -> list[Explorer] :
+
+def get_all() -> list[Explorer]:
     qry = "select * from explorer"
     curs.execute(qry)
     return [row_to_model(row) for row in curs.fetchall()]
 
+
 def create(explorer: Explorer) -> Explorer:
+    if not explorer:
+        return None
     qry = """insert into explorer (name, country, description)
-            values (:name, :country, :description)"""
+             values (:name, :country, :description)"""
     params = model_to_dict(explorer)
     try:
         curs.execute(qry, params)
     except IntegrityError:
-        raise Duplicate(msg=
-                        f"Explorer {explorer.name} already exists")    
+        raise Duplicate(msg=f"Explorer {explorer.name} already exists")
     return get_one(explorer.name)
+
 
 def modify(name: str, explorer: Explorer) -> Explorer:
     if not (name and explorer):
         return None
-    qry = """
-        update explorer
-        set country = :country,
-        name = :name,
-        description = :description
-        where name = :name_orig """
+    qry = """update explorer
+             set country=:country,
+             name=:name,
+             description=:description
+             where name=:name_orig"""
     params = model_to_dict(explorer)
     params["name_orig"] = explorer.name
     curs.execute(qry, params)
     if curs.rowcount == 1:
         return get_one(explorer.name)
     else:
-        raise Missing(msg="Explorer {name} not found")
-    
-def delete(name: str):
+        raise Missing(msg=f"Explorer {name} not found")
+
+
+def replace(name: str, explorer: Explorer) -> Explorer:
+    if not (name and explorer):
+        return None
+    qry = """update explorer
+             set country=:country,
+             name=:name,
+             description=:description
+             where name=:name_orig"""
+    params = model_to_dict(explorer)
+    params["name_orig"] = explorer.name
+    curs.execute(qry, params)
+    if curs.rowcount == 1:
+        return get_one(explorer.name)
+    else:
+        raise Missing(msg=f"Explorer {name} not found")
+
+
+def delete(name: str) -> bool:
     if not name:
         return False
     qry = "delete from explorer where name = :name"
-    params = {"name" : name}
+    params = {"name": name}
     curs.execute(qry, params)
     if curs.rowcount != 1:
         raise Missing(msg=f"Explorer {name} not found")
     return True
-
