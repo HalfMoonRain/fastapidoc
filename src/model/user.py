e64ff84aec56a4721fb9903d6e6a76bd4c87b655
junwoo <junwoo1357@gmail.com>
2024-08-13 22:53:53 +0900

    service user

---
+++ src/model/user.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel
+
+class PublicUser(BaseModel):
+    name: str
+
+class SignInUser(PublicUser):
+    password: str
+
+class PrivateUser(PublicUser):
+    hash: str
+    
