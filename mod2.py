61ae2ac95079f9dbc694bde7f72b73fefd402e25
junwoo <junwoo1357@gmail.com>
2024-08-19 22:07:34 +0900

    fake test

--- mod2.py
+++ mod2.py
@@ -1,4 +1,9 @@
-import mod1
+import os
+
+if os.getenv("UNIT_TEST"):
+    import fake_mod1 as mod1
+else:
+    import mod1
 
 def summer(x: int, y: int) -> str:
     return mod1.preamble() + f"{x+y}"
