0db72a822f67d55a434a60f07cb4930be0e37721
junwoo <junwoo1357@gmail.com>
2024-08-19 21:57:22 +0900

    test summer 2
     - simple test with mock

---
+++ test_summer2.py
@@ -0,0 +1,22 @@
+from unittest import mock
+
+import mod1
+import mod2
+
+def test_caller_a():
+    with mock.patch("mod1.preamble", return_value=""):
+        assert "11" == mod2.summer(5, 6)
+
+def test_caller_b():
+    with mock.patch("mod1.preamble") as mock_preamble:
+        mock_preamble.return_value = ""
+        assert "11" == mod2.summer(5, 6)
+
+@mock.patch("mod1.preamble", return_value="")
+def test_caller_c(mock_preamble):
+    assert "11" == mod2.summer(5, 6)
+
+@mock.patch("mod1.preamble")
+def test_caller_d(mock_preamble):
+    mock_preamble.return_value = ""
+    assert "11" == mod2.summer(5, 6)
