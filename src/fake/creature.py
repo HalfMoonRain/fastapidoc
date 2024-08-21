24b0db080961babf136a92e2cbafb963b7e2eb38
junwoo <junwoo1357@gmail.com>
2024-08-21 20:04:02 +0900

    fake creature

--- src/fake/creature.py
+++ src/fake/creature.py
@@ -1,3 +1,4 @@
+from error import Duplicate, Missing
 from src.model.creature import Creature
 
 # 데이터베이스와 SQL로 바꾸기 전 까지 사용 할 가짜 데이터
@@ -19,28 +20,38 @@ def get_all() -> list[Creature]:
     """생명체 목록을 반환한다."""
     return _creatures
 
-def get_one(name: str) -> Creature | None :
+def get_one(name: str) -> Creature:
     """ 검색한 생명체를 반환한다."""
     for _creature in _creatures:
         if _creature.name == name:
             return _creature
-    return None
-
-# 다음 함수는 현재 올바로 작동하지 않는다.
-# 실제로는 _creatures 목록을 수정하지 않는다.
-# 하지만 장동 하는 것처럼 동작한다.
+    return Missing(msg=f"Creature {name} not found")
 
 def create(creature: Creature) -> Creature:
     """생명체를 추가한다."""
+    if next((x for x in _creatures if x.name == creature.name), None):
+        raise Duplicate(msg=f"Creature {creature.name} already exists")
+    _creatures.append(creature)
     return creature
 
 def modify(name: str, creature: Creature) -> Creature:
     """생명체 정보를 일부 수정한다."""
-    return creature
-
+    _creature = next((x for x in _creatures if x.name == creature.name), None)
+    if _creatures is not None:
+        _creature = creature
+        return _creature
+    else:
+        raise Missing(msg=f"Creature {name} not Found")
+    
 def replace(name:str, creature: Creature) -> Creature:
     """생명체를 교체한다."""
-    return creature
+    _creature = next((x for x in _creatures if x.name == creature.name), None)
+    if _creature is None:
+        raise Missing(msg=f"Creature {name} not found")
+    
+    _creature = creature
+    return _creature
+
 
 def delete(name: str) -> bool:
     """생명체를 삭제한다. 만약 대상이 없다면 False를 반환한다."""
